97e3b3be27810afb32e4968bdaa5fc746a948f14
johannes <hjohannnes@gmail.com>
2021-01-08 16:48:32 +0100

    mathe funktionen

--- app/src/main/java/com/example/myapplication2/math.kt
+++ app/src/main/java/com/example/myapplication2/math.kt
@@ -17,6 +17,13 @@ class math : AppCompatActivity() {
         ausgabe += "E: %.20f\n".format(E)
         ausgabe += "PI: %.20f\n\n".format(PI)
 
+        ausgabe += "Wurzel, Exponential- und Logarithmusfunktionen:\n"
+        ausgabe += "5.2.pow(3.5): %f\n".format(5.2.pow(3.5))
+        ausgabe += "log(320.6, 5.2): %f\n".format(log(320.6, 5.2))
+        ausgabe += "sqrt(7.5): %f\n".format(sqrt(7.5))
+        ausgabe += "ln(100.0): %f\n".format(ln(100.0))
+        ausgabe += "exp(4.6): %f\n\n".format(exp(4.6))
+
         val textView: TextView = findViewById(R.id.textView)
         textView.text = ausgabe
 
